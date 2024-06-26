024ebcbda79e690d97aca21799515dc44f8bf739
Marcelo Tomporoski Perez <marcelo.perez@ifms.edu.br>
2024-06-26 09:14:12 -0400

    aula 25-06

---
+++ js/script.js
@@ -0,0 +1,64 @@
+/*
+elabora em 25 de junho de 2024
+script principal do frontend que acessa 
+os itens dos menus comas suas respectivas págians html
+Marcelo perez
+*/
+//
+function menu(item){
+    $("#conteudo").hide();  
+    $( "#conteudo" ).load( "paginas/"+item+".html").fadeTo(200,0.8);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* 
+
+function menu(item) {
+    var href = "paginas/"+item + ".html";
+    $.ajax({
+        url: href,
+        dataType: 'html',
+        success: function (html) {
+            $('#conteudo').html(html);
+        }
+    })
+} */
+
+
+
+
+
+
+
+
+
+
+
+
+/* 
+$(document).on("click", "a", function () {
+    var href = $(this)[0].attributes[1].value;
+    $.ajax({
+        url: href,
+        dataType: 'html',
+        success: function (html) {
+            $('#conteudo').html(html)
+        }
+    })
+    return false;
+}); */
